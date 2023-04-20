d2a7cd701401d24355686b5427fe7f13ee803343
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-04-20 10:39:09 +0200

    first two migrations

---
+++ database/migrations/2023_04_20_082137_create_caves_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('caves', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',80);
+            $table->string('location',120);
+            $table->float('latitude',20,15);
+            $table->float('longitude',20,15);
+            $table->smallInteger('deep',2500);
+            $table->smallInteger('length',25000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('caves');
+    }
+};
---
+++ database/migrations/2023_04_20_083158_create_activities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cave_id');
+            $table->foreign('cave_id')
+                ->references('id')
+                ->on('caves');
+            $table->string('object',255);
+            $table->string('target',255);
+            $table->text('report');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+};
