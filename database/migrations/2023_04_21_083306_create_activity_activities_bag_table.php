5e6fd4ed9df11ce173b660932b9cec4bd3a9c79c
albertobaggio96 <albertobaggio96@gmail.com>
2023-04-21 10:34:12 +0200

    pivot activity bag

---
+++ database/migrations/2023_04_21_083306_create_activity_activities_bag_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_activities_bag', function (Blueprint $table) {
+            $table->unsignedBigInteger('activity_id');
+            $table->foreign('activity_id') 
+                ->references('id')
+                ->on('activities')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('bag_id');
+            $table->foreign('bag_id')
+                ->references('id')
+                ->on('bags')
+                ->onDelete('cascade')
+                ->onDelete('cascade');
+
+            $table->smallInteger('amount');
+
+            $table->primary(['activity_id', 'bag_id']);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity_activities_bag');
+    }
+};
