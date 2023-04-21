e23f0efbf4499b01a5c87946f29eda837384f1b3
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-04-21 12:13:17 +0200

    create Member model

---
+++ app/Models/members/Member.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\members;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'subscription_year'
+    ];
+}
