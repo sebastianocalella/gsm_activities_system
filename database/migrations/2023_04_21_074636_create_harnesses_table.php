d9fee35432386726d1dec5057fcd432458f48caa
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-04-21 10:18:24 +0200

    HOTFIX removed harnesses table useless row

--- database/migrations/2023_04_21_074636_create_harnesses_table.php
+++ database/migrations/2023_04_21_074636_create_harnesses_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('harnesses', function (Blueprint $table) {
             $table->id();
-            $table->tinyInteger('stock_number');
             $table->timestamps();
         });
     }
